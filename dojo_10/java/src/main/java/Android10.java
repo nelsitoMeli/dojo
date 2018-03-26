c093259e183b6ba486b1f8318aface3875fb35be
Nelson Puglisi <nelson.puglisi@mercadolibre.com>
2018-03-26 09:59:44 -0300

    fix

--- dojo_10/java/src/main/java/Android10.java
+++ dojo_10/java/src/main/java/Android10.java
@@ -5,6 +5,7 @@ public class Android10 extends Cliente {
     @Override
     public void transform(Congrat congrats) {
         congrats.removeSection("exit");
+
         congrats.replaceOfflinePaymentType("offline_payment");
     }
 }
