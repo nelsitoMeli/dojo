afc9a834a51184eb22bc45ddd4eaaaed1c38b247
Nelson Puglisi <nelson.puglisi@mercadolibre.com>
2018-06-01 09:53:31 -0300

    kotlin version

---
+++ dojo_12/kotlin/app/src/test/kotlin/com/dojo/unlm/DojoTest.kt
@@ -0,0 +1,26 @@
+package com.dojo.unlm
+
+import org.junit.Assert
+import org.junit.Test
+
+import org.junit.Assert.*
+import org.junit.Before
+
+/**
+ * Tests for the dojo.
+ */
+class DojoTest {
+
+    @Before
+    fun setup() {
+
+    }
+
+    @Test
+    fun test_ejemplo() {
+
+        Assert.assertTrue(true)
+        Assert.assertEquals(B(), B())
+    }
+
+}
